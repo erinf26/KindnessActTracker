d3a601809f6449b20813807e6ff063c74fb9c877
Erin F <erin.foley@yale.edu>
2024-01-21 16:38:07 -0500

    Initial Commit

---
+++ KindnessApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  KindnessApp
+//
+//  Created by Erin Foley on 1/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ KindnessApp/KindnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  KindnessAppApp.swift
+//  KindnessApp
+//
+//  Created by Erin Foley on 1/21/24.
+//
+
+import SwiftUI
+
+@main
+struct KindnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
